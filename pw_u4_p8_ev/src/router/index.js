7daaa6cbe04cb584594d0b3ed33f2b6113350d86
Emil2245 <emilmontiel@hotmail.com>
2025-07-16 19:42:20 -0500

    Taller36

--- pw_u4_p8_ev/src/router/index.js
+++ pw_u4_p8_ev/src/router/index.js
@@ -1,31 +1,55 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import EstudianteView from '@/views/EstudianteView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import EstudianteView from "@/views/EstudianteView.vue";
+import LoginView from "@/views/LoginView.vue";
+
+// function estaAutenticado(){
+//   //comprobacion de la flag auth
+//   return localStorage.getItem('auth')==='true';
+// }
+
+const estaAutenticado = () => localStorage.getItem("auth") === "true";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/home",
+    name: "home",
+    component: HomeView,
+    meta: {
+      //nombre personalizable
+      requiresAuth: true, //protegida
+    },
   },
+  // {
+  //   path: "/estudiante",
+  //   name: "estudiante",
+  //   component: EstudianteView,
+  // },
+
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: "/login",
+    name: "login",
+    component: LoginView,
   },
-  {
-    path: '/estudiante',
-    name: 'estudiante',
-    component: EstudianteView
-  }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+//GUARDIAN
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    //en caso de no estar autenticado
+    if (!estaAutenticado()) {
+      next("/login");
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
